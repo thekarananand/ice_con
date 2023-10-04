95b2bf4e595ca273109c5724cdf187240d8b1123
Karan Anand <thekarananand@gmail.com>
2023-10-04 15:05:23 +0530

    Started with Nav

--- components/footerarea.jsx
+++ components/footerarea.jsx
@@ -5,8 +5,11 @@ const FooterArea = () => {
       <a href="">Link 2</a>
       <a href="">Link 3</a>
       <a href="">Link 4</a>
+      <div>
+        Designed & Developed in-house by <a href="">Student Coordinators Committee</a>
+      </div>
     </footer>
   )
 }
 
-export default FooterArea
+export default FooterArea 
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,9 +1,24 @@
 
 const NavBar = () => {
   return (
-    <header>
-      <div>Department of Instrumentation & Control Engineering</div>
-      <div>Dr. B R Ambedkar National Institute of Technology, Jalandhar</div>
+    <header >
+      <nav>
+        <div className="left">
+          <h2>Department of Instrumentation & Control Engineering</h2>
+          <div>Dr. B R Ambedkar National Institute of Technology, Jalandhar</div>
+        </div>
+
+        <div className="right">
+          <ul>
+            <li>About</li>
+            <li>Call for Papers</li>
+            <li>Committee</li>
+            <li>Programs</li>
+            <li>Hotels & Travel</li>
+          </ul>
+        </div>
+
+      </nav>
     </header>
   )
 }
