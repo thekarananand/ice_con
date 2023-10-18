195b017b307acd987a5d3a6e7b2eb6d2c83128c4
Karan Anand <thekarananand@gmail.com>
2023-10-18 18:15:13 +0530

    Restructured Project

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
+# Website for E2A Conference!
 
 ## Stack used
 
 - Development Framework : **NEXT.JS**
-- Database : **MongoDB**
 
 ## Spin the Development Server 
 
-- Download the Node Modules 
+- **Step 1**: Download the Node Modules 
 
     ``` bash
     npm install
     ```
 
-- Run the following to start the Development Server 
+- **Step 2**: Start the Development Server 
 
     ``` bash
     npm run dev
     ```
 
-- The Website will be available at [localhost:3000](http://localhost:3000) 
+- **Step 3**: The Website will be available at [localhost:3000](http://localhost:3000) 
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,64 @@
 import HomePage from './page'
 import NavBar from '@/components/navbar'
 import FooterArea from '@/components/footerarea'
-   
+        
 import '@/styles/global.css'
 
 export const metadata = {
-  title: "ICE CONFERENCE'24",
-  description: "ICE CONFERENCE'24"
+    title: "ICE CONFERENCE'24",
+    description: "ICE CONFERENCE'24"
 }
 
 const RootLayout = () => {
-  return (
-    <html lang='en'>
-      <body>
+    return (
+        <html lang='en'>
+            <body>
         
-        <NavBar/>
-        <HomePage/>
-        <FooterArea/>
+                {/* <NavBar/> */}
+
+                <main>
+                    <section>
+                        <div className="data">
+                            HERO SECTION
+                        </div>
+                    </section>
+                    <section>
+                        <div className="data">
+                            ABOUT #WELCOME
+                        </div>
+                    </section>
+                    <section>
+                        <div className="data">
+                            IMPORTANT DATES
+                        </div>
+                    </section>
+                    <section>
+                        <div className="data">
+                            CALL FOR PAPERS
+                        </div>
+                    </section>
+                    <section>
+                        <div className="data">
+                            SUBMIT
+                        </div>
+                    </section>
+                    <section>
+                        <div className="data">
+                            SPONSERS
+                        </div>
+                    </section>
+                    <section>
+                        <div className="data">
+                            VENUE
+                        </div>
+                    </section>
+                </main>
+      
+                {/* <FooterArea/> */}
         
-      </body>
-    </html>
-  )
+            </body>
+        </html>
+    )
 }
 
 export default RootLayout
