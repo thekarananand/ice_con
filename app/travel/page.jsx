8b363d139ca591fdfe76917a891d76d895974edb
skgarg18 <kgarg3112@gmail.com>
2024-03-29 23:15:34 +0530

    travel page file

--- app/travel/page.jsx
+++ app/travel/page.jsx
@@ -1,24 +1,68 @@
-import dm_sans from '@/components/TheFont'
+'use client'
 
-import TheHeroSection from '@/components/TheHeroSection'
+import Trav from '@/components/travel'
+
+import { useState  } from 'react';
+
+import dm_sans from '@/components/TheFont'
+import TheHeroSection from '@/components/TheHeroSection';
+
 
 const PageContent = () => {
+
+    const name = "wonderland , jalandhar"
+
+    const [widths, setWidths] = useState([2, 35, 2 ,2]);
+
+    const maxWidth = Math.max(...widths);
+    const minWidth = Math.min(...widths);
+
+    const handleMouseOver = (index) => {
+
+      
+      const newWidths = widths.map((w, i) => (i === index ? maxWidth : minWidth));
+      setWidths(newWidths);
+
+    };
+    
+
     return (
         <main className={dm_sans.className}>
             <TheHeroSection
                 smallText=''
                 largeText="Hotels, Travels & Accommodation"
             />
+
             <div className="S1 SkewSection">
                 <section className="content-container">
                     <div className="content">
-                        <h2>Note: This is a Placeholder Information!</h2>
+                        {/* <h2>Note: This is a Placeholder Information!</h2>
                         <p>
                             The International Conference on Electrical, Electronics, and Automation (E2A) serves as a global hub where experts gather to explore, collaborate, and share groundbreaking research and innovations in the rapidly evolving fields of electrical engineering, electronics, and automation. E2A provides a vital platform for diverse professionals to exchange ideas and knowledge, fostering innovation and shaping a future powered by cutting-edge technologies. Join us at E2A to be part of this transformative journey towards a  smarter, more connected, and automated world, where your contributions are essential in shaping the future. Selected papers will be published in SCOPUS indexed proceedings
                         </p>
                     </div>
-                </section>
+                    <div> */}
+
+                    
+            <div className='flex' id='container'>
+            {widths.map((wid, index) => (
+                <Trav
+                  key={index}
+                  wid={widths[index]}
+                  onmouseover={() => handleMouseOver(index)}
+                  locationName = {name}
+                />
+             ))}
+                    </div>
+                        
+                    </div>
+                    
+
+                </section>       
             </div>
+
+            
+            
         </main>
     )
 }
---
+++ components/travel.jsx
@@ -0,0 +1,59 @@
+
+import '@/styles/travel.css';
+
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiamondTurnRight } from '@fortawesome/free-solid-svg-icons'
+
+import { useState } from 'react';
+
+import bg_img from '../public/location/download.jpeg'
+
+const trav=({wid = "" , onmouseover , locationName})=>{
+
+    let visib ;
+
+    
+
+    if(wid ==35){
+        visib = "visible"; 
+    }
+    else{
+        visib = "hidden";
+    }
+
+
+
+    return(
+        <section id='travel_sec' style={{width : `${wid}%`  }} onMouseOver = {onmouseover} >
+            <div id="box" >
+            <Image
+                id="bgimg"
+                className="HeroImg"
+                src={bg_img}
+                priority={true}
+                alt="A Picture of NIT Jalandhar"
+            />
+
+                <div id="text" style={{visibility : `${visib}`}} >
+
+                    <div>
+                        <p>Place Name</p>
+                        <p>description</p>
+                        <div id='icon'>
+                            <a href= {`https://www.google.com/maps?q=${encodeURIComponent(locationName)}`} target="_blank">
+                            <FontAwesomeIcon icon={faDiamondTurnRight} />
+                            </a>
+                        </div>
+                        
+                    </div>
+
+                    
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default trav;
